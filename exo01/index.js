7588790f54e811b75f588ed4673f1c44b1c5c1fc
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-25 12:21:05 +0100

    Create IU for Exo 01

---
+++ exo01/index.js
@@ -0,0 +1,18 @@
+const ReadlinePromise = require('./modules/readline-promise');
+
+// ↓ On souhaite utilise le mot clef "await"
+const exo01 = async () => {
+    const reader = new ReadlinePromise();
+
+    let stop = false;
+    while (!stop) {
+        const firstname = await reader.question('Votre prénom');
+        const lastname = await reader.question('Votre nom');
+
+        // TODO Save data in file :D 
+
+        const nextValue = await reader.question('Voulez vous continuer (y/N) ?');
+        stop = nextValue.toLowerCase() !== 'y';
+    }
+};
+exo01();
